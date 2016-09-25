7c2a05174c32912433d1f4ed5c3d7ea258359786
nodza <noel.hwande@gmail.com>
2016-09-25 14:11:48 -0500

    Added search functionality

--- app/components/profile.component.ts
+++ app/components/profile.component.ts
@@ -10,7 +10,15 @@ import 'rxjs/add/operator/map';
 export class ProfileComponent {
   user[];
   repos[];
+  username: string;
+
   constructor(private  _githubService:GithubService) {
+    this.user = false;
+  }
+
+  searchUser() {
+    this._githubService.updateUser(this.username);
+
     this._githubService.getUser().subscribe(user => {
       // console.log(user)
       this.user = user;
